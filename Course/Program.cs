dca06bc5dc30ee345b4326a1a18833d6a95a026d
rafael <romilson.2r@gmail.com>
2021-04-12 20:57:20 -0300

    first commit

---
+++ Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "alex", Email = "alex@gmail.com" };
+
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b); // comparação de referencia de memoria
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+}
